833887ef83940d2bc58f352f928b1a61a1fc0bcc
Sara <Sara@SARA>
2024-12-15 21:54:42 +0000

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,203 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtID = New System.Windows.Forms.TextBox()
+        Me.txtMarca = New System.Windows.Forms.TextBox()
+        Me.txtModelo = New System.Windows.Forms.TextBox()
+        Me.btnPesquisa = New System.Windows.Forms.Button()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.btnInserir = New System.Windows.Forms.Button()
+        Me.btnAtualizar = New System.Windows.Forms.Button()
+        Me.btnEliminar = New System.Windows.Forms.Button()
+        Me.btnUpload = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Label4 = New System.Windows.Forms.Label()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(37, 279)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(21, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "ID:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(37, 336)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(40, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Marca:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(37, 409)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(45, 13)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Modelo:"
+        '
+        'txtID
+        '
+        Me.txtID.Location = New System.Drawing.Point(88, 271)
+        Me.txtID.Name = "txtID"
+        Me.txtID.Size = New System.Drawing.Size(100, 20)
+        Me.txtID.TabIndex = 4
+        '
+        'txtMarca
+        '
+        Me.txtMarca.Location = New System.Drawing.Point(88, 336)
+        Me.txtMarca.Name = "txtMarca"
+        Me.txtMarca.Size = New System.Drawing.Size(162, 20)
+        Me.txtMarca.TabIndex = 5
+        '
+        'txtModelo
+        '
+        Me.txtModelo.Location = New System.Drawing.Point(88, 402)
+        Me.txtModelo.Name = "txtModelo"
+        Me.txtModelo.Size = New System.Drawing.Size(162, 20)
+        Me.txtModelo.TabIndex = 6
+        '
+        'btnPesquisa
+        '
+        Me.btnPesquisa.Location = New System.Drawing.Point(270, 271)
+        Me.btnPesquisa.Name = "btnPesquisa"
+        Me.btnPesquisa.Size = New System.Drawing.Size(75, 23)
+        Me.btnPesquisa.TabIndex = 7
+        Me.btnPesquisa.Text = "Pesquisar"
+        Me.btnPesquisa.UseVisualStyleBackColor = True
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Location = New System.Drawing.Point(379, 240)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(242, 182)
+        Me.PictureBox2.TabIndex = 8
+        Me.PictureBox2.TabStop = False
+        '
+        'btnInserir
+        '
+        Me.btnInserir.Location = New System.Drawing.Point(664, 271)
+        Me.btnInserir.Name = "btnInserir"
+        Me.btnInserir.Size = New System.Drawing.Size(75, 23)
+        Me.btnInserir.TabIndex = 9
+        Me.btnInserir.Text = "Inserir"
+        Me.btnInserir.UseVisualStyleBackColor = True
+        '
+        'btnAtualizar
+        '
+        Me.btnAtualizar.Location = New System.Drawing.Point(664, 326)
+        Me.btnAtualizar.Name = "btnAtualizar"
+        Me.btnAtualizar.Size = New System.Drawing.Size(75, 23)
+        Me.btnAtualizar.TabIndex = 10
+        Me.btnAtualizar.Text = "Atualizar"
+        Me.btnAtualizar.UseVisualStyleBackColor = True
+        '
+        'btnEliminar
+        '
+        Me.btnEliminar.Location = New System.Drawing.Point(664, 383)
+        Me.btnEliminar.Name = "btnEliminar"
+        Me.btnEliminar.Size = New System.Drawing.Size(75, 23)
+        Me.btnEliminar.TabIndex = 11
+        Me.btnEliminar.Text = "Eliminar"
+        Me.btnEliminar.UseVisualStyleBackColor = True
+        '
+        'btnUpload
+        '
+        Me.btnUpload.Location = New System.Drawing.Point(474, 450)
+        Me.btnUpload.Name = "btnUpload"
+        Me.btnUpload.Size = New System.Drawing.Size(75, 23)
+        Me.btnUpload.TabIndex = 12
+        Me.btnUpload.Text = "Upload"
+        Me.btnUpload.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(40, 533)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(717, 237)
+        Me.DataGridView1.TabIndex = 13
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 36.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(203, 66)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(430, 55)
+        Me.Label4.TabIndex = 14
+        Me.Label4.Text = "Stand Automóveis"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 782)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.btnUpload)
+        Me.Controls.Add(Me.btnEliminar)
+        Me.Controls.Add(Me.btnAtualizar)
+        Me.Controls.Add(Me.btnInserir)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.btnPesquisa)
+        Me.Controls.Add(Me.txtModelo)
+        Me.Controls.Add(Me.txtMarca)
+        Me.Controls.Add(Me.txtID)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtID As TextBox
+    Friend WithEvents txtMarca As TextBox
+    Friend WithEvents txtModelo As TextBox
+    Friend WithEvents btnPesquisa As Button
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents btnInserir As Button
+    Friend WithEvents btnAtualizar As Button
+    Friend WithEvents btnEliminar As Button
+    Friend WithEvents btnUpload As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Label4 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,53 @@
+﻿Imports System.Data.SqlClient
+Imports System.IO
+Imports System.Drawing.Imaging
+Public Class Form1
+
+    Dim con As SqlConnection = New SqlConnection("Data Source=(localdb\MSSQLLocalDB;Database=24023;Integrated Security=true;")
+    Public Sub executarQuery(MyCommand As SqlCommand, MyMessage As String)
+        ' criar uma sub-procedure com o nome executarQuery que permite executar tarefas numa BD
+        ' Dim command As New SqlCommand(query, con)
+        con.Open() ' abre uma ligação à BD
+        If MyCommand.ExecuteNonQuery = 1 Then
+            MessageBox.Show(MyMessage)
+        Else
+            MessageBox.Show("Dados Não inseridos")
+        End If
+        con.Close() ' fecha a ligação à BD
+
+    End Sub
+
+    Private Sub btnInserir_Click(sender As Object, e As EventArgs) Handles btnInserir.Click
+        Dim ms As MemoryStream
+        imgImagem.Image.Save(ms, imgImagem.Image.Rawformat)
+        Dim Img() As Byte
+        Img = ms.ToArray()
+
+        Dim insertQuery As String = "Insert INTO Stand(Marca,Modelo,Foto)Values('" & txtMarca.Text & "','" & txtModelo.Text & "', @img)"
+
+        Dim command As New SqlCommand(insertQuery, con)
+        command.Parameters.Add("@img", SqlDbType.Image).Value = Img
+
+        executarQuery(command, "Dados e imagem inseridas com sucesso")
+        txtMarca.Text = ""
+        txtModelo.Text = ""
+
+    End Sub
+
+    Private Sub btnUpload_Click(sender As Object, e As EventArgs) Handles btnUpload.Click
+        Dim opf As New OpenFileDialog
+
+        opf.Filter = "Choose Image(*.JPG, *.PNG, *.GIF)|*.jpg;*.png;*.gif"
+
+        If opf.ShowDialog = Windows.Forms.DialogResult.OK Then
+            PictureBox2.Image = Image.FromFile(opf.FileName)
+        End If
+
+        Dim ms As New MemoryStream
+        PictureBox2.Image.Save(ms, PictureBox2.Image.RawFormat)
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+End Class
